a48c8910f6b58bc69beb21f87b608ec57326c517
Justin-Sherfey <jsherfey505@gmail.com>
2022-02-22 20:28:27 -0800

    added delete, update to posts

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,3 +5,5 @@ const url = 'http://localhost:5000/posts/';
 // utilize axios to connect to mongodatabase server utilizing URI
 export const featchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
